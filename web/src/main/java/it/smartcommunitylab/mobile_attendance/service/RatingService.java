f5dbac4f06537465f116b2b5344853dfac37922a
mirkoperillo <mirko.perillo@gmail.com>
2018-05-04 14:50:58 +0200

    log the rating

---
+++ web/src/main/java/it/smartcommunitylab/mobile_attendance/service/RatingService.java
@@ -0,0 +1,8 @@
+package it.smartcommunitylab.mobile_attendance.service;
+
+import it.smartcommunitylab.mobile_attendance.model.Rating;
+
+public interface RatingService {
+
+    Rating saveRating(Rating rating);
+}
